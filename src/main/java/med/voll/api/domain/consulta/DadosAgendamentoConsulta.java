6ab49f5213f2adaffec4d00b654916c7213a6c40
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-03-20 11:51:57 -0300

    Adicionando json property e jsonalias

--- src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -2,14 +2,18 @@ package med.voll.api.domain.consulta;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 
 public record DadosAgendamentoConsulta(
 		
+		    @JsonAlias({"id_medico","medico_id"})
 			Long idMedico,
 			
 			@NotNull
+			@JsonAlias({"id_paciente","paciente_id"})
 			Long idPaciente,
 			
 			@NotNull
--- src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -1,8 +1,15 @@
 package med.voll.api.domain.consulta;
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record DadosDetalhamentoConsulta(
-			Long id, Long idMedico, Long idPaciente, LocalDateTime data
+			Long id, 
+			@JsonProperty("id_medico")
+			Long idMedico, 
+			@JsonProperty("id_paciente")
+			Long idPaciente, 
+			LocalDateTime data
 		) {
 
 }
